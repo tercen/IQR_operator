3736bf0a8acb926f5059281fbf93fa88155d382f
Alexandre Gouy <contact@agouy.com>
2020-07-10 19:54:05 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,9 +1,15 @@
 library(tercen)
 library(dplyr)
 
+iqr <- function(y) {
+  qts <- quantile(y, probs = c(0.25, 0.75), names = FALSE)
+  iqr <- diff(qts)
+  return(iqr)
+}
+
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
   group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  summarise(iqr = iqr(.y)) %>%
   ctx$addNamespace() %>%
   ctx$save()
